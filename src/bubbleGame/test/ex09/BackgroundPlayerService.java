e653a7a54aabf912f7034d82aebfda8dd9d705b9
dola258 <dola258@naver.com>
2022-01-25 21:58:04 +0900

    물방울 생성(움직임 x)

---
+++ src/bubbleGame/test/ex09/BackgroundPlayerService.java
@@ -0,0 +1,73 @@
+package bubbleGame.test.ex09;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+// 메인 스레드는 바쁘다 - 키보드 이벤트를 처리하기 바쁘다
+// 새로운 스레드 - 백그라운드에서 플레이어를 계속 관찰
+public class BackgroundPlayerService implements Runnable{
+
+	private BufferedImage image;
+	private Player player;
+	
+	public BackgroundPlayerService(Player player) {
+		this.player = player;
+		try {
+			image = ImageIO.read(new File("image/BackgroundMapService.png"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	@Override
+	public void run() {
+		// 색상 확인
+		while(true) {
+			
+			Color leftcolor = new Color(image.getRGB(player.getX() -10, player.getY() + 25));
+			Color rightcolor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+		//	System.out.println("왼쪽 색상 : " + leftcolor);
+		//	System.out.println("오른쪽 색상 : " + rightcolor);
+			
+			// 캐릭터 아래에서 좌우로 조금 좁게
+			int bottomColor = image.getRGB(player.getX() + 10, player.getY() + 50 + 5)
+					+ image.getRGB(player.getX() + 50 - 10, player.getY() + 50 + 5);
+			
+			// 바닥 충돌 확인
+			if(bottomColor != -2) {
+			//	System.out.println(bottomColor);
+			//	System.out.println("바닥에 충돌함!!!");
+				player.setDown(false);
+			} else {
+				if(!player.isUp() && !player.isDown()) {
+					player.down();
+				}
+			}
+			
+			// 좌우 외벽 충돌 확인
+			if(leftcolor.getRed() == 255 && leftcolor.getGreen() == 0 && leftcolor.getBlue() == 0) {
+			//	System.out.println("왼쪽 벽에 충돌 발생!!!!");
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			} else if(rightcolor.getRed() == 255 && rightcolor.getGreen() == 0 && rightcolor.getBlue() == 0) {
+			//	System.out.println("오른쪽 벽에 충돌 발생!!!!");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+			} else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
+			}
+		
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
---
+++ src/bubbleGame/test/ex09/BubbleFrame.java
@@ -0,0 +1,94 @@
+package bubbleGame.test.ex09;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+	
+	private JLabel backgroundMap;
+	private Player player;
+	
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();  // 이벤트 리스너
+		setVisible(true);
+	}
+	
+	private void initObject() {
+		backgroundMap = new JLabel(new ImageIcon("image/BackgroundMap.png"));
+
+	//	크기 조절
+	//	backgroundMap.setSize(1000,600); 
+	//  위치 조절	
+	//	backgroundMap.setLocation(300, 300);
+	//  JFrame에 JLabel이 그려진다 
+	//	add(backgroundMap); 
+		
+	//  JPanel 자체를 JLabel(backgroundMap)으로 변경하기	
+		setContentPane(backgroundMap);
+	
+		player = new Player(); // player new하기
+		add(player);           // 배경에 캐릭터 추가하기
+	}
+	
+	private void initSetting() {
+		setSize(1000, 640);
+		getContentPane().setLayout(null);
+		setLocationRelativeTo(null); // null (화면 가운데에 뜬다)
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌 때 JVM도 같이 종료하기
+	}
+	
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+			
+			// 키보드가 눌러졌을 때 이벤트 핸들러
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if(!player.isLeft() && !player.isLeftWallCrash()) {
+						player.left();
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(!player.isRight() && !player.isRightWallCrash()) {
+						player.right();
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(!player.isUp() && !player.isDown()) {
+						player.up();
+					}
+					break;
+				case KeyEvent.VK_SPACE:
+					Bubble bubble = new Bubble(player);
+					add(bubble);
+;					break;
+				}
+			}
+		
+			// 키보드가 떨어졌을 때 이벤트 핸들러
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+		}); 		
+	}
+	
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+}
